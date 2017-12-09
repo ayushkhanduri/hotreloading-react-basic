a438241713d5aeec506fb3cc51bdfec5f6d9ec1e
ayush <ayushkhanduri@gmail.com>
2017-12-09 10:14:58 +0530

    Working state

--- src/app/presentational/Login/LoginUI.component.js
+++ src/app/presentational/Login/LoginUI.component.js
@@ -5,21 +5,11 @@ import {Link} from 'react-router';
 const Page = ({title}) => (
 	<div>
 		<div className="App-header">
-        	<img className="App-logo" alt="logo" />
         	<h2>{title}</h2>
       	</div>
       	<p className="App-intro">
         	This is the {title} page.
       	</p>
-	  	<p>
-        	<Link to="/">Home</Link>
-      	</p>
-    	{/* <p>
-    	  	<Link to="/about">About</Link>
-      	</p>
-      	<p>
-       		<Link to="/settings">Settings</Link>
-      	</p> */}
     </div>
 ); 
 
